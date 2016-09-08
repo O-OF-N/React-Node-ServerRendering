48aa48cc718ba372f56abde2d6f3b31c71c1ba40
vinodm1986 <vinodm1986@gmail.com>
2016-09-08 10:35:55 -0400

    first

--- src/UI/utils/constants.js
+++ src/UI/utils/constants.js
@@ -5,7 +5,7 @@ export const OBSERVATIONS_ERROR = 'OBSERVATIONS_ERROR';
 export const GLUCOSE_CODE = "2345-7";
 
 
-export const OBSERVATIONS_FETCH_URL = "https://diabeties.herokuapp.com/results/glucose";
+export const OBSERVATIONS_FETCH_URL = "https://diabetesdashboard.herokuapp.com/results/glucose";
 export const AUTHORIZATION_HEADER = {Accept:"application/json+fhir"};
 
 

0ad6c4b9951946fdf274bc47663055e9a88419ca
vinodm1986 <vinodm1986@gmail.com>
2016-09-11 00:46:20 -0400

    access token

---
+++ dist/server/app.js
@@ -0,0 +1,71 @@
+'use strict';
+
+var _viewRendererController = require('./controllers/view-renderer-controller');
+
+var _viewRendererController2 = _interopRequireDefault(_viewRendererController);
+
+var _glucoseResultsController = require('./controllers/glucose-results-controller');
+
+var _glucoseResultsController2 = _interopRequireDefault(_glucoseResultsController);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// controller imports
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/../../public')));
+
+app.use('/', _viewRendererController2.default);
+app.use('/results', _glucoseResultsController2.default);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function (err, req, res) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+module.exports = app;
+//# sourceMappingURL=app.js.map
---
+++ dist/server/models/models.js
@@ -0,0 +1,36 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.POSTHeader = exports.AuthorizationHeader = exports.AccessTokenBody = exports.Observation = undefined;
+
+var _immutable = require('immutable');
+
+var _constants = require('../util/constants');
+
+var Constants = _interopRequireWildcard(_constants);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+var Observation = exports.Observation = new _immutable.Record({
+    date: '',
+    quantity: 0,
+    interpretation: ''
+});
+
+var AccessTokenBody = exports.AccessTokenBody = new _immutable.Record({
+    grant_type: Constants.GRANT_TYPE,
+    code: '',
+    redirect_uri: Constants.REDIRECT_URL,
+    client_id: Constants.CLIENT_ID
+});
+
+var AuthorizationHeader = exports.AuthorizationHeader = new _immutable.Record({
+    headers: Constants.AUTHORIZATION_HEADER
+});
+
+var POSTHeader = exports.POSTHeader = new _immutable.Record({
+    "Content-Type": "x-www-form-urlencoded"
+});
+//# sourceMappingURL=models.js.map

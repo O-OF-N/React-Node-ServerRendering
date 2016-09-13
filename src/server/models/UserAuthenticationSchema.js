3e30682727605e1431ce144a4663a4cb705b0c31
vinodm1986 <vinodm1986@gmail.com>
2016-09-13 14:19:03 -0400

    cleaning up

--- src/server/models/UserAuthenticationSchema.js
+++ src/server/models/UserAuthenticationSchema.js
@@ -35,6 +35,8 @@ const saveHelper = function* (userAuthentication) {
 
 const updateHelper = function* (_id, $set) {
     const userAuth = yield UserAuthenticationModel.findByIdAndUpdate({ _id }, { $set });
+    console.log('userauth>>>');
+    console.log(userAuth);
     return userAuth;
 }
 

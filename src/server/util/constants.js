b6d0ce89e3d6edc882b93734cc1004df291c43d0
vinodm1986 <vinodm1986@gmail.com>
2016-10-06 14:33:11 -0400

    first

--- src/server/util/constants.js
+++ src/server/util/constants.js
@@ -5,11 +5,12 @@ export const OBSERVATIONS = "Observation";
 export const AUTHORIZATION_HEADER = { Accept: "application/json+fhir" };
 
 export const LONIC_URL = "http://loinc.org|";
-export const LONIC_CODES = new Map(["glucose", "2345-7"],
+/*export const LONIC_CODES = new Map(["glucose", "2345-7"],
     ["weight", "8335-2"],
     ["height", "3137-7"],
     ["hemoglobin", "718-7"],
     ["bmi", "59574-4"]);
+*/
 
 export const OBSERVATIONS_FETCH_URL = 
 "https://fhir.sandboxcernerpowerchart.com/d075cf8b-3261-481d-97e5-ba6c48d3b41f/Observation?patient=1316024&code=http://loinc.org|2345-7,http://loinc.org|8335-2,http://loinc.org|3137-7,http://loinc.org|718-7,http://loinc.org|59574-4";

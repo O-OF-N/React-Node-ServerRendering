2653facd335f07cf0e233b5c4de73c0c34ada3a4
vinodm1986 <vinodm1986@gmail.com>
2016-10-07 19:17:38 -0400

    rebuild

--- src/UI/components/glucose-results/glucose-results-action.js
+++ src/UI/components/glucose-results/glucose-results-action.js
@@ -1,29 +1,30 @@
 import * as Constants from '../../utils/constants';
 import axios from 'axios';
 import * as Records from '../../records/records';
+import co from 'co';
 
 
 export const fetchGlucose = (state) => dispatch => {
-    dispatch({
-        type: Constants.GLUCOSE_FETCHING
-    });
-    axios.get(Constants.GLUCOSE_FETCH_URL.concat(`/${state}`), { headers: Constants.AUTHORIZATION_HEADER })
-        .then((glucoseList) => {
-            try {
-                const data = (glucoseList && glucoseList.data) ? glucoseList.data : null;
-                if (data) {
-                    const glucoseObj = data.map(glucose => new Records.Glucose(glucose));
-                    dispatch({ type: Constants.GLUCOSE_FETCHED, payLoad: glucoseObj });
-                } else
-                    throw { message: "glucose list is not fetched" };
-            } catch (err) {
-                const errImmutable = err ? new Records.HttpError(err.message) : new Records.HttpError();
-                dispatch({ type: Constants.GLUCOSE_ERROR, payLoad: errImmutable });
-            }
-        })
+    co(fetchGlucoseHelper.bind(null, state))
         .catch((err) => {
             const errImmutable = err ? new Records.HttpError(err) : new Records.HttpError();
             dispatch({ type: Constants.GLUCOSE_ERROR, payLoad: errImmutable });
         });
 
+};
+
+const fetchGlucoseHelper = function* (state) {
+    dispatch({ type: Constants.GLUCOSE_FETCHING });
+    const glucoseList = yield axios.get(Constants.GLUCOSE_FETCH_URL.concat(`/${state}`), { headers: Constants.AUTHORIZATION_HEADER });
+    try {
+        const data = (glucoseList && glucoseList.data) ? glucoseList.data : null;
+        if (data) {
+            const glucoseObj = data.map(glucose => new Records.Glucose(glucose));
+            dispatch({ type: Constants.GLUCOSE_FETCHED, payLoad: glucoseObj });
+        } else
+            throw { message: "glucose list is not fetched" };
+    } catch (err) {
+        const errImmutable = err ? new Records.HttpError(err.message) : new Records.HttpError();
+        dispatch({ type: Constants.GLUCOSE_ERROR, payLoad: errImmutable });
+    }
 };

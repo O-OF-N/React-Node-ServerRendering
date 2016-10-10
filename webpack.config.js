de1a8cb6f1461bcb5270f8184cfc848b99ed703c
vinodm1986 <vinodm1986@gmail.com>
2016-10-10 16:24:21 -0400

    rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -29,11 +29,11 @@ module.exports = {
         new webpack.NoErrorsPlugin(),
         new webpack.optimize.UglifyJsPlugin({
             compress: { warnings: false }
-        })/*,
+        }),
         new webpack.DefinePlugin({
             'process.env': {
                 'NODE_ENV': '"production"'
             }
-        })*/
+        })
     ]
 };

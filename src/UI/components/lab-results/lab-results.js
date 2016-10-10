7828e5bb5b72210b82313ff14aa738b47536c326
vinodm1986 <vinodm1986@gmail.com>
2016-10-10 15:03:34 -0400

    rebuild

--- src/UI/components/lab-results/lab-results.js
+++ src/UI/components/lab-results/lab-results.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import LabResultsHeader from './lab-results-header';
 import LabResultsBody from './lab-results-body';
+import {connect} from 'react-redux';
 
 const LabResults = ({labs}) => {
     return (

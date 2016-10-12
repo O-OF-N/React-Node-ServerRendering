ec7d07ff6c43311cafc99c23b15aaef280a333b4
vinodm1986 <vinodm1986@gmail.com>
2016-10-12 09:47:22 -0400

    rebuild

--- src/UI/components/diabetes-chart.js
+++ src/UI/components/diabetes-chart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import GlucoseResults from './glucose-results/glucose-results';
 import LabResults from './lab-results/lab-results';
-import Medications from './lab-results/lab-results';
+import Medications from './medications/medications';
 
 const DiabetesChart = () => (
     <div>

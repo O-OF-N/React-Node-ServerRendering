cb3be85aeb25da692bd33323538bae55efb4d6ac
vinodm1986 <vinodm1986@gmail.com>
2016-10-13 11:55:21 -0400

    rebuild

--- src/UI/components/lab-results/lab-results-body.js
+++ src/UI/components/lab-results/lab-results-body.js
@@ -7,7 +7,7 @@ import {
 
 const LabResultsBody = ({text, interpretation, quantity, date}) => (
     <div style={LabItemsStyle}>
-        <span style={LabFirstItemsSpanStyle title={text}}>{text ? text : '-'}</span>
+        <span style={LabFirstItemsSpanStyle} title={text}>{text ? text : '-'}</span>
         <span style={LabItemsSpanStyle} title = {interpretation}>{interpretation ? interpretation : '-'}</span>
         <span style={LabItemsSpanStyle} title = {quantity}>{quantity ? quantity : '-'}</span>
         <span style={LabItemsSpanStyle} title = {date ? new Date(date).toLocaleDateString() : '-'}>{date ? new Date(date).toLocaleDateString() : '-'}</span>

9bc0d16e46674de510f5f88f2f6a9b6f2dc6f1a6
vinodm1986 <vinodm1986@gmail.com>
2016-10-28 16:16:48 -0400

    rebuild

--- src/UI/components/diabetes-chart.js
+++ src/UI/components/diabetes-chart.js
@@ -2,16 +2,19 @@ import React from 'react';
 import GlucoseResults from './glucose-results/glucose-results';
 import LabResults from './lab-results/lab-results';
 import Medications from './medications/medications';
-import {inheritStyle} from './styles';
+import { inheritStyle } from './styles';
 
 const DiabetesChart = () => (
-    <div style ={inheritStyle}>
-        <div style ={{ width: '100%', height: '50%' }}>
-            <GlucoseResults/>
-            <LabResults/>
+    <div style={inheritStyle}>
+        <div style={{ width: '100%', height: '50%' }}>
+            <GlucoseResults />
+            <LabResults />
         </div>
-        <div style ={{ width: '100%', height: '50%' }}>
-            <Medications/>
+        <div style={{ width: '100%', height: '40%' }}>
+            <Medications />
+        </div>
+        <div style={{ width: '100%', height: '10%' }}>
+            <input type="button"> Sliding scale </input>
         </div>
     </div>
 );

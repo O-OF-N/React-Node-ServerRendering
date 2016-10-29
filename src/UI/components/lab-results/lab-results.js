90f08e6c8a020e320fc1a860719c62318e3798cd
vinodm1986 <vinodm1986@gmail.com>
2016-10-29 19:14:16 -0400

    rebuild

--- src/UI/components/lab-results/lab-results.js
+++ src/UI/components/lab-results/lab-results.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import LabResultsHeader from './lab-results-header';
 import LabResultsBody from './lab-results-body';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 const LabResults = ({labs}) => {
-    const style = { border: '1px solid', width: '48%', height: '100%', float: 'left',marginLeft:'0.5%', padding:'0.5%'};
+    const style = { border: '1px solid', width: '48%', height: '100%', float: 'left', marginLeft: '0.5%', padding: '0.5%' };
     return (
-        <div style = {style}>
+        <div style={style}>
             <h3>Diabetes Labs(Most recent two results captured in the past 24hrs)</h3>
-            <LabResultsHeader/>
+            <LabResultsHeader />
             <div id="div-lab-body">
                 {
-                   // labs.map((l, i) => <LabResultsBody key={i} {...l.toJS() }/>)
+                    labs.map((l, i) => <LabResultsBody key={i} {...l.toJS() } />)
                 }
             </div>
         </div>

32045ca9080c597f492bc087cbaa6bcf41a60c71
vinodm1986 <vinodm1986@gmail.com>
2016-10-29 19:16:40 -0400

    rebuild

--- src/UI/components/lab-results/lab-results.js
+++ src/UI/components/lab-results/lab-results.js
@@ -3,7 +3,7 @@ import LabResultsHeader from './lab-results-header';
 import LabResultsBody from './lab-results-body';
 import { connect } from 'react-redux';
 
-const LabResults = ({labs}) => {
+const LabResults = ({labMaps}) => {
     const style = { border: '1px solid', width: '48%', height: '100%', float: 'left', marginLeft: '0.5%', padding: '0.5%' };
     return (
         <div style={style}>
@@ -11,12 +11,12 @@ const LabResults = ({labs}) => {
             <LabResultsHeader />
             <div id="div-lab-body">
                 {
-                    labs.map((l, i) => <LabResultsBody key={i} {...l.toJS() } />)
+                    labMaps.map((l, i) => <LabResultsBody key={i} {...l.toJS() } />)
                 }
             </div>
         </div>
     )
 };
 export default connect(state => ({
-    labs: state.labObject.labs
+    labMaps: state.labObject.labMaps
 }))(LabResults);

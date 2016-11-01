1d397871322f3fb7c5a1474d692148d5d4e83102
vinodm1986 <vinodm1986@gmail.com>
2016-11-01 11:45:55 -0400

    rebuild

--- src/UI/components/medications/medications.js
+++ src/UI/components/medications/medications.js
@@ -33,10 +33,10 @@ const Medications = ({medications}) => {
     console.log('medications =  ' + medications);
     return (
         <div style={{ width: '96%', height: 'auto', padding: '1%', display: 'inline-flex', marginTop: '0.5%' }}>
-            {medications ? medications.filter(medication.type === 'Bolus / Sliding Scale').map(medication =>
+            {medications ? medications.filter(medication => medication.type === 'Bolus / Sliding Scale').map(medication =>
                 <BolusMedications medication={medication} />
             ) : null}
-            {medications ? medications.filter(medication.type !== 'Bolus / Sliding Scale').map(medication =>
+            {medications ? medications.filter(medication => medication.type !== 'Bolus / Sliding Scale').map(medication =>
                 <NonBolusMedications medication={medication} />
             ) : null}
 

8042c66a634424efb80c262c7c45ae9a257ec66c
vinodm1986 <vinodm1986@gmail.com>
2016-11-01 07:12:27 -0400

    rebuild

--- src/UI/components/medications/medications.js
+++ src/UI/components/medications/medications.js
@@ -1,29 +1,23 @@
 import React from 'react';
 import MedicationsHeader from './medications-header';
 import MedicationsBody from './medications-body';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 const Medications = ({medications}) => {
     return (
-        <div style = {{width: '96%',height: 'auto',padding: '1%',display: 'inline-flex',marginTop: '0.5%'}}>
-            <div style={{ float: 'left', width:'49%', paddingLeft:'0.5%' }}>
-                <h3>INTRAVENOUS</h3>
-                <MedicationsHeader/>
-                <div id="div-med-body-1">
-                    {
-                        medications.filter(m => m.administration === 'INTRAVENOUS').map((m, i) => <MedicationsBody key={i} {...m.toJS() }/>)
-                    }
+        <div style={{ width: '96%', height: 'auto', padding: '1%', display: 'inline-flex', marginTop: '0.5%' }}>
+            {medications ? medications.map(medication =>
+                <div style={{ float: 'left', width: '33%', paddingLeft: '0.5%' }}>
+                    <h3>medication.type</h3>
+                    <MedicationsHeader />
+                    <div id="div-med-body">
+                        {
+                            medication.medications.map((m, i) => <MedicationsBody key={i} {...m.toJS() } />)
+                        }
+                    </div>
                 </div>
-            </div>
-            <div style={{ float: 'left', width:'49%', paddingLeft:'0.5%' }}>
-                <h3>SUBCUTANEOUS</h3>
-                <MedicationsHeader/>
-                <div id="div-med-body-2">
-                    {
-                        medications.filter(m => m.administration === 'SUBCUTANEOUS').map((m, i) => <MedicationsBody key={i} {...m.toJS() }/>)
-                    }
-                </div>
-            </div>
+            ) : null}
+
         </div>
     )
 };

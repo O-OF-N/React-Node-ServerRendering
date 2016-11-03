a3f040610c2167ee5f779ccb5a143ec50f9b246e
vinodm1986 <vinodm1986@gmail.com>
2016-11-02 21:40:08 -0400

    rebuild

--- src/UI/components/medications/bolus/medications-bolus-table.js
+++ src/UI/components/medications/bolus/medications-bolus-table.js
@@ -14,6 +14,6 @@ const columns = [{
 }];
 
 const BolusMedicationsTable = ({medication}) =>
-    <Table columns={columns} data={medication} className="table" />
+    (<Table columns={columns} data={medication} className="table" />);
 
 export default BolusMedicationsTable;

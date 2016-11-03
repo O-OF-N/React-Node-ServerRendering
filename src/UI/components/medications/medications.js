f0cca7a10fed0f785ea36aab383c62a4bf83b93e
vinodm1986 <vinodm1986@gmail.com>
2016-11-03 01:54:30 -0400

    rebuild

--- src/UI/components/medications/medications.js
+++ src/UI/components/medications/medications.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import MedicationsHeader from './medications-header';
-import MedicationsBody from './medications-body';
-import BolusMedicationsHeader from './bolus/medications-bolus-header';
-import BolusMedicationsBody from './bolus/medications-bolus-body';
 import BolusMedicationsTable from './bolus/medications-bolus-table';
 import MedicationsTable from './medications-table';
 import { connect } from 'react-redux';
-import { MedicationTableStyle, MedicationBodyDivStyle } from '../styles';
+import { MedicationTableStyle } from '../styles';
 
 
 const NonBolusMedications = ({medication}) => {
     const med = medication ? medication.medications.map(m => m.toJS()) : null;
     return (
         <div style={MedicationTableStyle}>
-            <MedicationsTable data={med} title = {medication.type}/>
+            <MedicationsTable data={med} title={medication.type} />
         </div>
     );
 };
@@ -21,7 +17,7 @@ const BolusMedications = ({medication}) => {
     const med = medication ? medication.medications.map(m => m.toJS()) : null;
     return (
         <div style={MedicationTableStyle}>
-            <BolusMedicationsTable data={med} title = {medication.type}/>
+            <BolusMedicationsTable data={med} title={medication.type} />
         </div>
     );
 };

887b383e1e16da8f1eef6815f6f5384512edfdfe
vinodm1986 <vinodm1986@gmail.com>
2016-11-03 09:38:38 -0400

    rebuild

--- src/UI/components/lab-results/lab-results-table.js
+++ src/UI/components/lab-results/lab-results-table.js
@@ -4,16 +4,41 @@ require('rc-table/assets/index.css');
 
 
 const columns = [{
-    lab
+    title: 'Lab', dataIndex: 'code', key: 'code', width: '24%',
 }, {
-    title: 'Dosage', dataIndex: 'dosage', key: 'dosage', width: '33%',
+    title: 'Result', dataIndex: 'quantity', key: 'quantity', width: '24%',
 }, {
-    title: 'Result', dataIndex: 'result', key: 'result', width: '33%',
-}, {
-    title: 'Date/Time', dataIndex: 'date', key: 'date', width: '33%'
+    title: 'Date', dataIndex: 'date', key: 'date', width: '24%',
+    render: (date) => dateFormat(date)
 }];
 
-const LabTable = ({data}) =>
-    (<Table columns={columns} data={data} className="table" scroll={{ y: '150' }} />);
+const LabTable = ({data}) => {
+    const dataBuilt = buildData(data);
+    return (<Table columns={columns} data={dataBuilt} className="table" scroll={{ y: '150' }} />);
+};
+
+const dateFormat = (date) => {
+    return { children: new Date(date).toLocaleString(), props: { colSpan: 1, rowSpan: 1 } }
+};
+
+const buildData = (data) => {
+    data.map(d => {
+        let lab1, lab2;
+        const code = d.code;
+        const labs = d.labs;
+        [lab1, lab2] = labs;
+        return {
+            code: code,
+            quantity: lab1.quantity,
+            date: lab1.date,
+            unit: lab1.unit,
+            children: [{
+                quantity: lab2.quantity,
+                date: lab2.date,
+                unit: lab2.unit
+            }]
+        };
+    });
+};
 
 export default LabTable;
--- src/UI/components/lab-results/lab-results.js
+++ src/UI/components/lab-results/lab-results.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import LabResultsHeader from './lab-results-header';
 import LabResultsBody from './lab-results-body';
+import LabTable from './lab-results-table';
 import { connect } from 'react-redux';
 import { LabDivStyle, LabBodyDivStyle } from '../styles';
 
 const LabResults = ({labMaps}) => (
     <div style={LabDivStyle}>
         <h3>Diabetes Labs(Most recent two results captured in the past 24hrs)</h3>
-        <LabResultsHeader />
-        <div id="div-lab-body" style={LabBodyDivStyle}>
-            {
-                labMaps.map((l, i) => <LabResultsBody key={i} code={l.code} labs={l.labs} />)
-            }
-        </div>
+        <LabTable data={labMaps} />
     </div>
 );
 

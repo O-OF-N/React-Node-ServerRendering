a16cc0d4d2e8b6148219e9400280e91c6e240124
vinodm1986 <vinodm1986@gmail.com>
2016-11-04 12:34:04 -0400

    rebuild

--- src/server/util/http-utils.js
+++ src/server/util/http-utils.js
@@ -8,15 +8,16 @@ export const buildAuthorizationHeader = (userModel) => {
 
 export const checkResponseStatus = (json) => (json && json.status && json.status === Constants.HTTP_SUCCESS) ? true : false;
 
-export const buildObeservationURL = (patient, lonicCodes, url,dates) => {
+//test patient ids: 4640007,4638007
+export const buildObeservationURL = (patient, lonicCodes, url, dates) => {
     const codes = lonicCodes.map(l => Constants.LONIC_URL.concat('|').concat(l)).join(',');
     let urlConstructed = '';
-    if(dates!= null && dates instanceof Array && dates.length==2){
+    if (dates != null && dates instanceof Array && dates.length == 2) {
         const dateRange = ''.concat('date=gt').concat(dates[0]).concat('&date=lt').concat(dates[1]);
-        urlConstructed= `${url}/${Constants.OBSERVATIONS}?patient=${4640007}&code=${codes}&${dateRange}`;
-    } else 
-    urlConstructed= `${url}/${Constants.OBSERVATIONS}?patient=${4640007}&code=${codes}`;
+        urlConstructed = `${url}/${Constants.OBSERVATIONS}?patient=${4638007}&code=${codes}&${dateRange}`;
+    } else
+        urlConstructed = `${url}/${Constants.OBSERVATIONS}?patient=${4638007}&code=${codes}`;
     return urlConstructed;
 };
 
-export const buildMedicationURL = (patient, url) => `${url}/${Constants.MEDICATION_ORDER}?patient=${4640007}&status=active`;
+export const buildMedicationURL = (patient, url) => `${url}/${Constants.MEDICATION_ORDER}?patient=${4638007}&status=active`;

c4f9b7dc519545cde6e03b59b5cd67c40460ace9
vinodm1986 <vinodm1986@gmail.com>
2016-11-04 09:05:43 -0400

    rebuild

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         }),
         new webpack.DefinePlugin({
             'process.env': {
-                'NODE_ENV': '"development"'
+                'NODE_ENV': '"production"'
             }
         })
     ]

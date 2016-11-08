c404a0c49407a1a8ec4c1d64562157f8397f14f6
vinodm1986 <vinodm1986@gmail.com>
2016-11-08 15:41:31 -0500

    rebuild

--- src/server/helper/medication-helper.js
+++ src/server/helper/medication-helper.js
@@ -94,6 +94,7 @@ const getAndMapRxNormIngredients = insulinOrders => {
 };
 
 const getRxNormIngredients = function*(rxNormCode) {
+    console.log('I reached here but not there>........');
     const rxnormdata = yield axios.get(`https://rxnav.nlm.nih.gov/REST/rxcui/${rxNormCode}/related?tty=IN+SBDC`);
     console.log('date>>>>>>>>>');
     console.log(rxnormdata);

c9dcc95bb2973a78568599d08da646b6919dfd1f
vinodm1986 <vinodm1986@gmail.com>
2016-11-08 15:54:37 -0500

    rebuild

--- src/server/helper/medication-helper.js
+++ src/server/helper/medication-helper.js
@@ -97,5 +97,6 @@ const getRxNormIngredients = function*(rxNormCode) {
     console.log('I reached here but not there>........');
     const rxnormdata = yield axios.get(`https://rxnav.nlm.nih.gov/REST/rxcui/${rxNormCode.code}/related?tty=IN+SBDC`);
     console.log('date>>>>>>>>>');
-    console.log(rxnormdata);
+    console.log(rxnormdata.data.relatedGroup.termType);
+    console.log(rxnormdata.data.relatedGroup.conceptGroup);
 }

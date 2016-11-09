38398b203cf095900ee456638bec6863a368172c
vinodm1986 <vinodm1986@gmail.com>
2016-11-09 16:01:43 -0500

    rebuild

--- src/server/helper/medication-helper.js
+++ src/server/helper/medication-helper.js
@@ -90,7 +90,7 @@ const categorizeOrders = function* (insulinOrders) {
 
 const getAndMapRxNormIngredients = function* (insulinOrders) {
     let insulin = [];
-   for(let insulinOrder of insulinOrders){
+   for(let insulinOrder in insulinOrders){
        for(let item of getRxNormIngredientsMapper(insulinOrder) ){
            insulin.push(item);
        }

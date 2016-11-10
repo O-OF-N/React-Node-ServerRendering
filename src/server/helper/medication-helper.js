32de9ba2ee671615cc385a49cfe49dd481b3e08e
vinodm1986 <vinodm1986@gmail.com>
2016-11-10 14:49:06 -0500

    rebuild

--- src/server/helper/medication-helper.js
+++ src/server/helper/medication-helper.js
@@ -3,7 +3,7 @@
 import * as Records from '../models/models';
 import { List } from 'immutable';
 import * as Constants from '../util/constants';
-import { get } from '../service/http-service'
+import { get, all } from '../service/http-service'
 import * as HttpUtil from '../util/http-utils';
 import UserAuthenticationModel from '../models/UserAuthenticationSchema';
 
@@ -86,7 +86,7 @@ const checkDosage = (value, order) => (value.dosage && value.dosage === order.ad
 const getIngredients = function* (insulinOrders) {
     try {
         const getFunctions = insulinOrders.map(insulinOrder => axiosGet.bind(null, insulinOrder.code)).toJS();
-        const ingredients = yield axios.all(getFunctions.map(fn => fn()));
+        const ingredients = yield all(getFunctions);
         const processedIngredients = ingredients.map(ingredient => processIngredients(ingredient));
         return insulinOrders.map((insulinOrder, index) => insulinOrder.merge({ ingredients: processedIngredients[index] }));
     } catch (err) {
@@ -100,11 +100,15 @@ const axiosGet = (code) => axios.get(`https://rxnav.nlm.nih.gov/REST/rxcui/${cod
 const processIngredients = rxNormData => {
     const ingredientsList = rxNormData.data.relatedGroup.conceptGroup.filter(group => group.tty === 'IN');
     const sbdcList = rxNormData.data.relatedGroup.conceptGroup.filter(group => group.tty === 'SBDC');
-    const response = ingredientsList && sbdcList && ingredientsList instanceof Array && sbdcList instanceof Array && ingredientsList.length > 0 && sbdcList.length > 0 && ingredientsList[0] && sbdcList[0] && sbdcList[0].conceptProperties && sbdcList[0].conceptProperties instanceof Array && sbdcList[0].conceptProperties.length > 0 ? { ingredients: ingredientsList[0], sbdcName: sbdcList[0].conceptProperties[0].name } : null;
-    const ingredients = response && response.ingredients && response.ingredients.conceptProperties ? response.ingredients.conceptProperties.map(conceptProperty => {
-        const code = { code: parseInt(conceptProperty.rxcui), name: conceptProperty.name };
-        return code;
-    }) : null;
+    const response = ingredientsList && sbdcList && ingredientsList instanceof Array && sbdcList instanceof Array &&
+        ingredientsList.length > 0 && sbdcList.length > 0 && ingredientsList[0] && sbdcList[0] && sbdcList[0].conceptProperties &&
+        sbdcList[0].conceptProperties instanceof Array && sbdcList[0].conceptProperties.length > 0 ?
+        { ingredients: ingredientsList[0], sbdcName: sbdcList[0].conceptProperties[0].name } : null;
+    const ingredients = response && response.ingredients && response.ingredients.conceptProperties ?
+        response.ingredients.conceptProperties.map(conceptProperty => {
+            const code = { code: parseInt(conceptProperty.rxcui), name: conceptProperty.name };
+            return code;
+        }) : null;
     return ingredients ? ingredients.length === 1 ? new Records.Ingredients({ codes: List([ingredients[0].code]), name: ingredients[0].name }) :
         new Records.Ingredients({ codes: List(ingredients.map(ingredient => ingredient.code)), name: response.sbdcName }) : null;
 };
--- src/server/service/http-service.js
+++ src/server/service/http-service.js
@@ -7,3 +7,5 @@ import qs from 'querystring';
 export const get = (url, header) => axios.get(url, header.toJS());
 
 export const post = (url, body, header) => axios.post(url, qs.stringify(body.toJS()), header.toJS());
+
+export const all = functions => axios.all(functions.map(fn => fn()));

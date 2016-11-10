62eec9fcecb11ce86619f65ac55331d572aca777
vinodm1986 <vinodm1986@gmail.com>
2016-11-10 01:21:23 -0500

    rebuild

--- src/server/helper/medication-helper.js
+++ src/server/helper/medication-helper.js
@@ -91,6 +91,7 @@ const categorizeOrders = function* (insulinOrders) {
 
 const getIngredients = function* (insulinOrders) {
     try {
+        console.log(insulinOrders);
         const getFunctions = insulinOrders.map(insulinOrder => axiosGet.bind(null, insulinOrder.code)).toJS();
         const ingredients = yield axios.all(getFunctions.map(fn => fn()));
         const processedIngredients = ingredients.map(ingredient => processIngredients(ingredient));

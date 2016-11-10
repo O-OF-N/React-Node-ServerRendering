fb3fb1e5b14bb37b3096c26cdb77770956bb78f3
vinodm1986 <vinodm1986@gmail.com>
2016-11-10 15:05:28 -0500

    rebuild

--- src/server/service/http-service.js
+++ src/server/service/http-service.js
@@ -4,7 +4,7 @@ import * as Constants from '../util/constants';
 import qs from 'querystring';
 
 
-export const get = (url, header) => axios.get(url, header.toJS());
+export const get = (url, header = null) => header ? axios.get(url, header.toJS()) : axios.get(url);
 
 export const post = (url, body, header) => axios.post(url, qs.stringify(body.toJS()), header.toJS());
 

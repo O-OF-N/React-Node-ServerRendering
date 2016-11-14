ad8f5f850754b9ee41cefb6c54edced2199a67ce
vinodm1986 <vinodm1986@gmail.com>
2016-11-14 10:59:24 -0500

    recompile

--- src/server/helper/medication-helper.js
+++ src/server/helper/medication-helper.js
@@ -27,7 +27,7 @@ export const fetchMedications = function* (state) {
 //Private functions
 const fetchMedicationsHelper = function* (state) {
     const [userAuthenticationModel] = yield UserAuthenticationModel.findByState(state);
-    if (!userAuthenticationModel) return new Exceptions.InvalidStateError(`State ${state} is invalid`);
+    if (!userAuthenticationModel) throw new Exceptions.InvalidStateError(`State ${state} is invalid`);
     const url = userAuthenticationModel ? HttpUtil.buildMedicationURL(userAuthenticationModel.patient, userAuthenticationModel.iss) : null;
     const authHeader = userAuthenticationModel ? HttpUtil.buildAuthorizationHeader(userAuthenticationModel) : null;
     return url && authHeader ? yield get(url, authHeader) : null;
--- src/server/util/exceptions.js
+++ src/server/util/exceptions.js
@@ -7,15 +7,21 @@ class ErrorRoot extends Error {
     }
 };
 
-export class InvalidStateError extends ErrorRoot {
+export class InvalidStateError {
     constructor(message) {
         super(message);
+    this.name = this.constructor.name;
+    this.message = message; 
+    Error.captureStackTrace(this, this.constructor.name);
     }
 };
 
 
-export class AuthenticationError extends ErrorRoot {
+export class AuthenticationError {
     constructor(message) {
         super(message);
+    this.name = this.constructor.name;
+    this.message = message; 
+    Error.captureStackTrace(this, this.constructor.name);
     }
 };

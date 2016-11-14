b474e247b13b346e1511f0acd41e89a3d84b2164
vinodm1986 <vinodm1986@gmail.com>
2016-11-14 10:47:15 -0500

    recompile

--- src/server/util/exceptions.js
+++ src/server/util/exceptions.js
@@ -1,20 +1,21 @@
 class ErrorRoot extends Error {
     constructor(message,name) {
-        super(message);
-        this.name = name;
-        this.message = message;
+    super(message);
+    this.name = this.constructor.name;
+    this.message = message; 
+    Error.captureStackTrace(this, this.constructor.name);
     }
 };
 
 export class InvalidStateError extends ErrorRoot {
     constructor(message) {
-        super(message, 'InvalidStateError');
+        super(message);
     }
 };
 
 
 export class AuthenticationError extends ErrorRoot {
     constructor(message) {
-        super(message, 'AuthenticationError');
+        super(message);
     }
 };

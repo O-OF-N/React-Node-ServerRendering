15d68c290607f050e92075096e770eaee32615bc
vinodm1986 <vinodm1986@gmail.com>
2017-01-26 16:33:35 -0500

    done

--- src/server/error-handler.js
+++ src/server/error-handler.js
@@ -16,7 +16,7 @@ export const DevErrorHandle = (err, req, res) => {
 export const ProdErrorHandle = (err, req, res) => {
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
+        message: 'Some sort of an error',
         error: {}
     });
 };

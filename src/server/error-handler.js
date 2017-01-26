a9f536729640fdf7dabfdaedce9d0db1298bb500
vinodm1986 <vinodm1986@gmail.com>
2017-01-26 16:00:42 -0500

    done

--- src/server/error-handler.js
+++ src/server/error-handler.js
@@ -4,7 +4,7 @@ var pattern = require("matches").pattern;
 // development error handler
 // will print stacktrace
 export const DevErrorHandle = (err, req, res) => {
-    console.log('I reached here some how. But dont now how',err)
+    console.log('I reached here some how. But dont now how', err)
     res.status(err.status || 500);
     res.render('error', {
         message: 'What are you trying here???'
@@ -14,28 +14,28 @@ export const DevErrorHandle = (err, req, res) => {
 // production error handler
 // no stacktraces leaked to user
 export const ProdErrorHandle = (err, req, res) => {
+    console.log('I reached here some how. But dont now how', err)
     res.status(err.status || 500);
     res.render('error', {
-        message: err.message,
-        error: {}
+        message: 'What are you trying here???'
     });
 };
 
 
 export const ErrorHandler = pattern({
     '"InvalidStateError",req,res,params': (req, res, params) =>
-        res.render('error',{
-        message: 'Are you cheating??',
-        error: {stack: 'Dont even try', status: 'caught a fraud'}
-    })
+        res.render('error', {
+            message: 'Are you cheating??',
+            error: { stack: 'Dont even try', status: 'caught a fraud' }
+        })
         /*res.status(500).send({
             message: 'Invalid authentication parameters sent'
         })*/,
     '"AuthenticationError",req,res,params': (req, res, params) => {
         console.log(`${req.hostname}/?iss=${params.iss}&launch=${params.launch}`);
-         res.render('error',{
-        message: 'Are you cheating??',
-        error: {stack: 'Dont even try', status: 'caught a fraud'}
-    })
+        res.render('error', {
+            message: 'Are you cheating??',
+            error: { stack: 'Dont even try', status: 'caught a fraud' }
+        })
     }
 });

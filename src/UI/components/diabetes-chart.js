2a33a2da527da073c5c4eab4f7ec7dad9b218615
vinodm1986 <vinodm1986@gmail.com>
2017-01-30 11:12:31 -0500

    rebuild

--- src/UI/components/diabetes-chart.js
+++ src/UI/components/diabetes-chart.js
@@ -9,6 +9,23 @@ import { inheritStyle } from './styles';
 const DiabetesChart = () => (
     <div style={inheritStyle}>
         <div style={{ width: '100%', height: '50%' }}>
+            <div className="dropdown">
+                <button className="btn" type="button" dataToggle="dropdown" ariaHaspopup="true" ariaExpanded="false">
+                    Default Dropdown
+    <span className="icon-decrement icon-fallback-img" ariaLabel="Decrement"></span>
+                </button>
+                <ul role="menu" ariaHidden="true">
+                    <li>
+                        <a href="#">Default Link One</a>
+                    </li>
+                    <li>
+                        <a href="#">Default Link Two</a>
+                    </li>
+                    <li>
+                        <a href="#">Default Link Three</a>
+                    </li>
+                </ul>
+            </div>
             <GlucoseResults />
             <LabResults />
         </div>
@@ -16,23 +33,7 @@ const DiabetesChart = () => (
             <Medications />
         </div>
         <div> <IBCModal /> </div>
-        <div className="dropdown">
-            <button className="btn" type="button" dataToggle="dropdown" ariaHaspopup="true" ariaExpanded="false">
-                Default Dropdown
-    <span className="icon-decrement icon-fallback-img" ariaLabel="Decrement"></span>
-            </button>
-            <ul role="menu" ariaHidden="true">
-                <li>
-                    <a href="#">Default Link One</a>
-                </li>
-                <li>
-                    <a href="#">Default Link Two</a>
-                </li>
-                <li>
-                    <a href="#">Default Link Three</a>
-                </li>
-            </ul>
-        </div>
+
     </div>
 
 

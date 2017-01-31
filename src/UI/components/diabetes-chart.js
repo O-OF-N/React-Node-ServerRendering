d13ca10795a026776618d9067a34229ba6740b8a
vinodm1986 <vinodm1986@gmail.com>
2017-01-31 15:44:31 -0500

    rebuild

--- src/UI/components/diabetes-chart.js
+++ src/UI/components/diabetes-chart.js
@@ -7,6 +7,23 @@ import { inheritStyle } from './styles';
 
 const DiabetesChart = () => (
     <div style={inheritStyle}>
+        <div class="dropdown">
+            <button class="btn" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Default Dropdown
+    <span class="icon-decrement icon-fallback-img" aria-label="Decrement"></span>
+            </button>
+            <ul role="menu" aria-hidden="true">
+                <li>
+                    <a href="#">Default Link One</a>
+                </li>
+                <li>
+                    <a href="#">Default Link Two</a>
+                </li>
+                <li>
+                    <a href="#">Default Link Three</a>
+                </li>
+            </ul>
+        </div>
         <div style={{ width: '100%', height: '50%' }}>
             <GlucoseResults />
             <LabResults />

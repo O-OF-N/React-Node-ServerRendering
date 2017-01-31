23bafaaa998c41137f4499c16ce8729621e2fd87
vinodm1986 <vinodm1986@gmail.com>
2017-01-31 00:50:16 -0500

    rebuild

--- src/UI/components/diabetes-chart.js
+++ src/UI/components/diabetes-chart.js
@@ -5,29 +5,9 @@ import Medications from './medications/medications';
 import IBCModal from './sliding-scale/IBC-Modal';
 import { inheritStyle } from './styles';
 
-const DiabetesChart = () =>{ 
-    console.log('here///////')
-    console.log(dropdown,btn);
-    return (
+const DiabetesChart = () => (
     <div style={inheritStyle}>
         <div style={{ width: '100%', height: '50%' }}>
-            <div className="dropdown">
-                <button className="btn" type="button" dataToggle="dropdown" ariaHaspopup="true" ariaExpanded="false">
-                    Default Dropdown
-    <span className="icon-decrement icon-fallback-img" ariaLabel="Decrement"></span>
-                </button>
-                <ul role="menu" ariaHidden="true">
-                    <li>
-                        <a href="#">Default Link One</a>
-                    </li>
-                    <li>
-                        <a href="#">Default Link Two</a>
-                    </li>
-                    <li>
-                        <a href="#">Default Link Three</a>
-                    </li>
-                </ul>
-            </div>
             <GlucoseResults />
             <LabResults />
         </div>
@@ -39,5 +19,5 @@ const DiabetesChart = () =>{
     </div>
 
 
-)};
+);
 export default DiabetesChart;

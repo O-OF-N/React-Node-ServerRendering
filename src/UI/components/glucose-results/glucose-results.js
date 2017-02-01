134006b52a37dc020ead1347a05274cdf64af9f6
vinodm1986 <vinodm1986@gmail.com>
2017-02-01 16:16:48 -0500

    rebuild

--- src/UI/components/glucose-results/glucose-results.js
+++ src/UI/components/glucose-results/glucose-results.js
@@ -27,7 +27,7 @@ class GlucoseResults extends React.Component {
         };
 
         const styleFull = {
-            width: '95%', float: 'left', paddingLeft: '0.5%', maxWidth: '100%',
+            width: '95%', height: '100%', float: 'left', paddingLeft: '0.5%', maxWidth: '100%',
             maxHeight: '100%',
             overflow: 'auto'
         };
@@ -36,7 +36,7 @@ class GlucoseResults extends React.Component {
         return (
             <div style={styleFull}>
                 <h3>Blood Glucose (all sources for past 24 hours)</h3>
-                <div id="chart"></div>
+                <div id="chart" style={{ width: '100%', height: '100%' }}></div>
             </div>
         )
     }

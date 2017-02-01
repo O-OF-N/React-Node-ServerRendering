236ab8d84f43752c4f838a2ea8fcba0ef246b64a
vinodm1986 <vinodm1986@gmail.com>
2017-02-01 11:17:58 -0500

    rebuild

--- src/UI/components/glucose-results/glucose-results.js
+++ src/UI/components/glucose-results/glucose-results.js
@@ -43,6 +43,15 @@ class GlucoseResults extends React.Component {
                 </div>*/}
 
                 <div id="chart"></div>
+                {c3.generate({
+                    bindto: '#chart',
+                    data: {
+                        columns: [
+                            ['data1', 30, 200, 100, 400, 150, 250],
+                            ['data2', 50, 20, 10, 40, 15, 25]
+                        ]
+                    }
+                })};
             </div>
         )
     }

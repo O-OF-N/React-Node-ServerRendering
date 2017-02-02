41425a90a63d612d65121ec0cedb4d69d48bf7fd
vinodm1986 <vinodm1986@gmail.com>
2017-02-02 15:51:14 -0500

    glucose

--- src/UI/components/diabetes-chart.js
+++ src/UI/components/diabetes-chart.js
@@ -7,6 +7,19 @@ import { inheritStyle } from './styles';
 
 const DiabetesChart = () => (
     <div style={inheritStyle}>
+
+        <section class="demographics-banner">
+            <header>
+                <div class="demographics-row">
+                    <h1>Doe, Jane</h1>
+                    <h1>St. John's Heroes Wing Bed Eight</h1>
+                </div>
+                <div class="demographics-row">
+                    <span><b>22 yrs</b> <b>F</b> DOB: <b>04/13/92</b></span>
+                    <span>MRN: <b>6789235423</b></span>
+                </div>
+            </header>
+        </section>
         <div style={{ width: '100%', maxHeight: '45%' }}>
             <GlucoseResults />
         </div>

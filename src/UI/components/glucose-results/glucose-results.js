839971e4b16a4d962dfefb42ba5d34b8ca3d7088
vinodm1986 <vinodm1986@gmail.com>
2017-02-02 14:13:58 -0500

    glucose

--- src/UI/components/glucose-results/glucose-results.js
+++ src/UI/components/glucose-results/glucose-results.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux';
 
 const timeStringForGraph = date => new Date(date).toLocaleTimeString([], { hour12: false, hour: '2-digit', minute: '2-digit' });
 
+const timeStringForTooltip = date => new Date(date).toLocaleTimeString([], { hour12: true });
+
 class GlucoseResults extends React.Component {
     constructor(props) {
         super(props);
@@ -14,7 +16,7 @@ class GlucoseResults extends React.Component {
             const glucose = this.props.glucose.filter(g => g.quantity != null && g.date != null).sort((g1, g2) => g1.date > g2.date);
             const data = ['Blood Glucose', ...glucose.map(g => g.quantity).toJS()];
             const labels = ['x', ...glucose.map(g => timeStringForGraph(g.date)).toJS()];
-            const toolTip = glucose.map(g => { return { date: g.quantity, toolTip: g.source } })
+            const toolTip = glucose.map((g, index) => { return { index: labels[index + 1], toolTipDate: timeStringForTooltip(g.date), source: g.source, value: g.value } })
             drawChart(labels, data, toolTip);
         }
     }

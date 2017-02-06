7e2fa37f08b4cd2e8458021a3990e6819504ca31
vinodm1986 <vinodm1986@gmail.com>
2017-02-06 16:25:42 -0500

    code

--- src/UI/client.js
+++ src/UI/client.js
@@ -18,6 +18,7 @@ import * as Constants from './utils/constants';
 import co from 'co';
 import { inheritStyle } from './components/styles';
 import './terra/css/terra.min.css';
+import './terra/img/icon_sprite.png';
 
 const logger = createLogger();
 

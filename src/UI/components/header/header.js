db28db6f5a6bc8d4eb0a16a3c7938a7652f7a2b6
vinodm1986 <vinodm1986@gmail.com>
2017-02-07 10:36:03 -0500

    glucose

--- src/UI/components/header/header.js
+++ src/UI/components/header/header.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
-const header = ({toggle}) => (<section style = {{backgroundColor:'blue'}}>
+const header = ({toggle}) => (<section style={{ backgroundColor: '#0092e0' }}>
     <header>
         <div>
-            <h1>Diabetes Management
-                    <button className="btn btn-default" onClick={toggle}>Insulin Bolus Calculator</button>
-            </h1>
+            <h1 style={{ color: 'white' }} >Diabetes Management</h1>
+            <button style={{ float: 'right' }} className="btn btn-default" onClick={toggle}>Insulin Bolus Calculator</button>
         </div>
     </header>
 </section>);

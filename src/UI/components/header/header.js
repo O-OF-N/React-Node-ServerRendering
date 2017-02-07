01b7f5ad2f24874d7d1691a860d39b57cab8f800
vinodm1986 <vinodm1986@gmail.com>
2017-02-07 15:53:35 -0500

    glucose

--- src/UI/components/header/header.js
+++ src/UI/components/header/header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './header.css';
+import Error from '../error/error';
 
 const header = ({toggle}) => (<section className="section">
     <header>
         <div>
             <h1 className="header-text">Diabetes Management</h1>
+            <Error />
             <span className="button-span">
                 <button style={{ marginRight: '10' }} className="btn btn-default ibc-button" onClick={toggle}>Insulin Bolus Calculator</button>
             </span>

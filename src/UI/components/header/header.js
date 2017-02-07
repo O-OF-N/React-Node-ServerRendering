8ad9c896da08343df43f51817b3d95d7bc996706
vinodm1986 <vinodm1986@gmail.com>
2017-02-07 11:22:57 -0500

    glucose

--- src/UI/components/header/header.js
+++ src/UI/components/header/header.js
@@ -5,7 +5,9 @@ const header = ({toggle}) => (<section style={{ backgroundColor: '#0092e0' }}>
     <header>
         <div>
             <h1 className="header-text">Diabetes Management</h1>
-            <button style={{ marginRight:'10' }} className="btn btn-default" onClick={toggle}>Insulin Bolus Calculator</button>
+            <span className="button-span">
+                <button style={{ marginRight: '10' }} className="btn btn-default ibc-button" onClick={toggle}>Insulin Bolus Calculator</button>
+            </span>
         </div>
     </header>
 </section>);

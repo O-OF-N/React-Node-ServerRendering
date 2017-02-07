a09eff554959f2a55f080884d783bbabd4dd1f0c
vinodm1986 <vinodm1986@gmail.com>
2017-02-07 15:44:26 -0500

    glucose

--- src/UI/components/error/error.js
+++ src/UI/components/error/error.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import '../../terra/vendor/jquery/jquery-2.2.0.min'
 import '../../terra/css/terra.css'
 import '../../terra/fonts/terra_icons/terra_icons.eot'
 import '../../terra/fonts/terra_icons/terra_icons.svg'
 import '../../terra/fonts/terra_icons/terra_icons.ttf'
 import '../../terra/img/icon_sprite.png'
-import '../../terra/js/terra'
 
 const error = () => (
     <div class="alert alert-error" role="alert">

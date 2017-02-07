cb112440799b5941c5f72e8e308e1ce20829df5d
vinodm1986 <vinodm1986@gmail.com>
2017-02-07 16:21:10 -0500

    glucose

--- src/UI/components/error/error.js
+++ src/UI/components/error/error.js
@@ -1,9 +1,4 @@
 import React from 'react';
-import '../../terra/css/terra.css'
-import '../../terra/fonts/terra_icons/terra_icons.eot'
-import '../../terra/fonts/terra_icons/terra_icons.svg'
-import '../../terra/fonts/terra_icons/terra_icons.ttf'
-import '../../terra/img/icon_sprite.png'
 
 const error = () => (
     <div className="alert alert-error" role="alert">

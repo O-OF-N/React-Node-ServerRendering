ca4ea9b09a38a6cea7aa122adef80a9b360d5984
vinodm1986 <vinodm1986@gmail.com>
2017-02-08 09:40:19 -0500

    glucose

--- src/UI/components/lab-results/lab-results-body.js
+++ src/UI/components/lab-results/lab-results-body.js
@@ -1,36 +1,15 @@
 import React from 'react';
 
-const LabResultValues = ({text, quantity, date, unit}) => (
-    <div style={LabValuesStyle}>
-        <span style={LabItemsSpanStyle} title={quantity}>{quantity ? quantity : '-'}{unit ? ` ${unit}` : ''}</span>
-        <span style={LabItemsSpanStyle} title={date ? new Date(date).toLocaleDateString() : '-'}>{date ? new Date(date).toLocaleString() : '-'}</span>
-    </div>
-);
+const buildRow = rowData => <tr>
+    <th scope="row">rowData.code</th>
+    <td>rowData.quantity1</td>
+    <td>rowData.quantity2</td>
+    <td>Content</td>
+</tr>
 
-const LabResultName = ({code}) => (
-    <span style={LabFirstItemsSpanStyle} title={code}>{code ? code : '-'}</span>
-);
-
-const LabResultsBody = ({code, labs}) => (
+const LabResultsBody = ({data}) => (
     <tbody>
-      <tr>
-        <th scope="row">Row Heading</th>
-        <td>Content</td>
-        <td>Content</td>
-        <td>Content</td>
-      </tr>
-      <tr>
-        <th scope="row">Row Heading</th>
-        <td>Content</td>
-        <td>Content</td>
-        <td>Content</td>
-      </tr>
-      <tr>
-        <th scope="row">Row Heading</th>
-        <td>Content</td>
-        <td>Content</td>
-        <td>Content</td>
-      </tr>
+        data.map(d1=>buildRow(d1));
     </tbody>
 )
 

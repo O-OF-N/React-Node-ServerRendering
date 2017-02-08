9ff21180fc5bf2415d4572b247c714d825f2f3df
vinodm1986 <vinodm1986@gmail.com>
2017-02-08 14:56:17 -0500

    fonts

--- src/UI/components/medications/medications-body.js
+++ src/UI/components/medications/medications-body.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import {
-    MedicationItemsStyle,
-    MedicationItemsSpanStyle,
-    MedicationFirstItemsSpanStyle
-} from '../styles';
 
-const MedicationResultsBody = ({prescriber, status, date, medication, dosage, administration}) => (
-    <div id="div-med-body" style={MedicationItemsStyle}>
-        <span style={MedicationFirstItemsSpanStyle} title = {medication} >{medication ? medication : '-'}</span>
-        <span style={MedicationItemsSpanStyle} title = {dosage} > {dosage ? dosage : '-'}</span>
-        <span style={MedicationItemsSpanStyle} title = {date ? new Date(date).toLocaleString() : '-'} > {date ? new Date(date).toLocaleString() : '-'}</span>
-    </div>
+/*const MedicationResultsBody = ({data}) => (
+    <tbody>
+        {data.map(d => )}
+    </tbody>
 );
+
+const buildRows= data => {
+    <tr>
+        <td>{data.}
+};*/
 
 export default MedicationResultsBody;
--- src/UI/components/medications/medications-table.js
+++ src/UI/components/medications/medications-table.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { MedicationTableStyle, MedicationBodyDivStyle } from '../styles';
-import Table from 'rc-table';
-require('rc-table/assets/index.css');
+import MedicationsHeder from './medications-header';
 
 
 const columns = [{
@@ -13,8 +12,13 @@ const columns = [{
     render: (date) => dateFormat(date)
 }];
 
-const MedicationsTable = ({data, title}) =>
-    (<Table columns={columns} data={data} className="table" scroll={{ y: '150' }} title={() => <div><h3>{title}</h3></div>} />);
+const MedicationsTable = ({data, title}) =>{
+    console.log(data);
+    console.log(title);
+    return (<table class="table-base table-striped">
+        <MedicationsHeader />
+    </table>);
+}
 
 const dateFormat = (date) => {
     return { children: new Date(date).toLocaleString(), props: { colSpan: 1, rowSpan: 1 } }

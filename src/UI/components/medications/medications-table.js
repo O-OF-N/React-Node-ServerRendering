af724bd1490cf1ea5980b875928cca39438538a6
vinodm1986 <vinodm1986@gmail.com>
2017-02-09 17:19:59 -0500

    first

--- src/UI/components/medications/medications-table.js
+++ src/UI/components/medications/medications-table.js
@@ -4,13 +4,13 @@ import MedicationsHeader from './medications-header';
 import MedicationBody from './medications-body';
 
 
-const MedicationsTable = ({data, title, comment}) => {
+const MedicationsTable = ({data, title, comments}) => {
     console.log(data);
     console.log(title);
     return (<table className="table-base table-striped">
         <caption><h5>{title}</h5></caption>
         <MedicationsHeader />
-        <MedicationBody data={data} comment={comment} />
+        <MedicationBody data={data} comments={comments} />
     </table>);
 }
 

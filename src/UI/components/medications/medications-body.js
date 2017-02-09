7001a0472a550f311ca6cdb6a668b974cbefdd18
vinodm1986 <vinodm1986@gmail.com>
2017-02-09 17:23:08 -0500

    first

--- src/UI/components/medications/medications-body.js
+++ src/UI/components/medications/medications-body.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './medications.css';
 
-const MedicationBody = ({data,comments}) => (
+const MedicationBody = ({data, comments}) => (
     <tbody>
-        {data.map(d => buildRows(d,comments))}
+        {data.map(d => buildRows(d, comments))}
     </tbody>
 );
 
-const buildRows = (data,comments) => (
+const buildRows = (data, comments) => (
     <tr>
         <td className="order-details"><span className="medication">{data.ingredients.name}</span>
             <span className="text">{data.dosage}</span>
-            {comments?<span className="text">{data.comments}</span>:null}</td>
+            {comments ? <span className="text"><br />{data.comments}</span> : null}</td>
         <td className="date data-type-date">{dateFormat(data.date)}</td>
     </tr>
 );

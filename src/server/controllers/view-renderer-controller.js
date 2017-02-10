99b2ee6e951b887a76483b646406e6a4c5b11258
vinodm1986 <vinodm1986@gmail.com>
2017-02-09 20:40:47 -0500

    first

--- src/server/controllers/view-renderer-controller.js
+++ src/server/controllers/view-renderer-controller.js
@@ -67,7 +67,6 @@ const renderFullPage = (html, state) => {
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="stylesheet" type="text/css" href="/terra/css/terra.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
         <script src='/terra/vendor/cldrjs/cldr.min.js' />
         <script src='/terra/vendor/fastclick/fastclick.min.js' />
         <script src='/terra/vendor/jquery/jquery.2.2.0.min.js' />

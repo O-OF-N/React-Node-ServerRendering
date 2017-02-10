1da2e1990cbad7876d278f368e7e07cfbd6f289b
vinodm1986 <vinodm1986@gmail.com>
2017-02-09 20:24:08 -0500

    first

--- src/server/controllers/view-renderer-controller.js
+++ src/server/controllers/view-renderer-controller.js
@@ -67,7 +67,6 @@ const renderFullPage = (html, state) => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <link rel="stylesheet" type="text/css" href="/terra/css/terra.min.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
-        <script src="http://code.jquery.com/ui/1.9.2/jquery-ui.js"></script>
         <script src='/terra/js/terra.js' />
         <script type="text/javascript">  
             function evaluate(x) {

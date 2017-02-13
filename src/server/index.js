c3637168e262acd69d56a81deb529298f0c1d707
vinodm1986 <vinodm1986@gmail.com>
2017-02-13 11:10:33 -0500

    first

--- src/server/index.js
+++ src/server/index.js
@@ -6,7 +6,7 @@ export default class Component extends React.Component {
         return (
             <div style={style}>
                 <div id="app" style={style}>
-                    <div className="loading loading-large" style={{ display: 'block', margin: '0 auto' }}>
+                    <div className="loading loading-large" style={{ display: 'block', margin: 'auto auto' }}>
                         <span className="icon-spinner" aria-hidden="true"></span> Loading
                         <marquee direction="right">&hellip;&nbsp;</marquee>
                     </div>

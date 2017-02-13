f70c22b462dd13708d880acf986249bbe1de4027
vinodm1986 <vinodm1986@gmail.com>
2017-02-13 14:26:51 -0500

    first

--- src/UI/components/diabetes-chart.js
+++ src/UI/components/diabetes-chart.js
@@ -11,12 +11,14 @@ import Header from './header/header';
 const toggle = dispatch => dispatch({ type: Constants.SLIDING_SCALE_TOGGLE_VISIBILITY });
 const styleHalf = {
     width: '58%', height: '100%', float: 'left', padding: '0.5%', maxWidth: '58%',
-    maxHeight: '100%'
+    maxHeight: '100%', backgroundColor: 'rgba(221, 223, 224, 0.35)',
+    boxShadow: 'inset 1px 0 0 0 #c8cacb'
 };
 
 const styleFull = {
     width: '95%', height: '100%', float: 'left', padding: '0.5%', maxWidth: '100%',
-    maxHeight: '100%'
+    maxHeight: '100%', backgroundColor: 'rgba(221, 223, 224, 0.35)',
+    boxShadow: 'inset 1px 0 0 0 #c8cacb'
 };
 const DiabetesChart = ({slidingScale, dispatch}) => (
     <div style={inheritStyle}>
@@ -25,15 +27,17 @@ const DiabetesChart = ({slidingScale, dispatch}) => (
             slidingScale.visible ?
                 <div style={{ width: '100%', maxHeight: '45%', minHeight: '45%' }}>
                     <GlucoseResults style={styleHalf} />
-                    <div style={{     backgroundColor: 'rgba(221, 223, 224, 0.35)',
-                                      boxShadow: 'inset 1px 0 0 0 #c8cacb',
-                                      width: '41%', overflow: 'auto', padding: '0.5%' }}> <IBC /> </div>
+                    <div style={{
+                        backgroundColor: 'rgba(221, 223, 224, 0.35)',
+                        boxShadow: 'inset 1px 0 0 0 #c8cacb',
+                        width: '41%', overflow: 'auto', padding: '0.5%'
+                    }}> <IBC /> </div>
                 </div>
                 : <div style={{ width: '100%', maxHeight: '45%', minHeight: '45%' }}>
                     <GlucoseResults style={styleFull} />
                 </div>
         }
-        <div style={{ width: '100%', maxHeight: '45%', marginTop: '3%',overflowY:'auto' }}>
+        <div style={{ width: '100%', maxHeight: '45%', marginTop: '3%', overflowY: 'auto' }}>
             <LabResults />
             <Medications />
         </div>

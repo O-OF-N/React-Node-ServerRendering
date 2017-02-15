ccacb3d5e4fc1ed543476f6741ec82985e798781
vinodm1986 <vinodm1986@gmail.com>
2017-02-15 17:40:52 -0500

    push

--- src/server/controllers/view-renderer-controller.js
+++ src/server/controllers/view-renderer-controller.js
@@ -67,6 +67,7 @@ const renderFullPage = (html, state) => {
     <title>Diabetes Management</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/terra/css/terra.min.css">
     <script type="text/javascript">
         function evaluate(x) {

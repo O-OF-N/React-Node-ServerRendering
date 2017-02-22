3ec30726ba55b69c4698647c26694ed398a51fcd
vinodm1986 <vinodm1986@gmail.com>
2017-02-22 16:42:16 -0500

    first

--- src/UI/components/glucose-results/chart/draw-chart.js
+++ src/UI/components/glucose-results/chart/draw-chart.js
@@ -2,49 +2,50 @@ import 'c3/c3.css';
 import 'd3';
 import * as c3 from 'c3/c3';
 
-const drawChart = (labels, data, tt) => c3.generate({
-    bindto: '#chart',
-    padding: {
-        top: 40,
-        right: 100,
-        bottom: 40,
-        left: 100,
-    },
-    data: {
-        x: 'x',
-        type: 'spline',
-        columns: [
-            labels,
-            data
-        ]
-    },
-    grid: {
-        y: {
-            show: true
-        }
-    },
-    axis: {
-        x: {
-            type: 'category'
+const drawChart = (labels, data, tt) => {
+    const toolTipArray = [].concat(tt);
+    return c3.generate({
+        bindto: '#chart',
+        padding: {
+            top: 40,
+            right: 100,
+            bottom: 40,
+            left: 100,
         },
-        y: {
-            label: {
-                text: 'Glucose Level (mg/dl)',
-                position: 'outer-middle'
+        data: {
+            x: 'x',
+            type: 'spline',
+            columns: [
+                labels,
+                data
+            ]
+        },
+        grid: {
+            y: {
+                show: true
+            }
+        },
+        axis: {
+            x: {
+                type: 'category'
+            },
+            y: {
+                label: {
+                    text: 'Glucose Level (mg/dl)',
+                    position: 'outer-middle'
+                }
+            }
+        },
+        tooltip: {
+            contents: function (d, defaultTitleFormat, defaultValueFormat, color) {
+                console.log('d=', d);
+                console.log('d[0]=', d[0]);
+                console.log('d[0].index=', d[0].index);
+                console.log('tt[d[0].index]=', tt[d[0].index]);
+                const content = tt[d[0].index]
+                return '<div id="tooltip"> Source: ' + content + ' <BR/> Value: ' + d[0].value + '</div>'
             }
         }
-    },
-    tooltip: {
-        contents: tooltip.bind(null, d, tt)
-    }
-});
-
-const tooltip = (d, tt) => {
-    console.log('d=', d);
-    console.log('d[0]=', d[0]);
-    console.log('d[0].index=', d[0].index);
-    console.log('tt[d[0].index]=', tt[d[0].index]);
-    const content = tt[d[0].index]
-    return '<div id="tooltip">' + content + '</div>'
-}
+    })
+};
 export default drawChart;

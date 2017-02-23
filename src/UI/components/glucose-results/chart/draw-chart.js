0f6650da9efdea0235a5cf48ba30c98d3b9f31f7
vinodm1986 <vinodm1986@gmail.com>
2017-02-23 11:51:28 -0500

    first

--- src/UI/components/glucose-results/chart/draw-chart.js
+++ src/UI/components/glucose-results/chart/draw-chart.js
@@ -4,7 +4,7 @@ import * as c3 from 'c3/c3';
 
 const ToolTip = (color, content, title, value) =>
     `<div style= "color: white">
-        <table style= "border: 1px solid #dedfe0 ">
+        <table style= "border: 1px solid;">
             <thead style= "background-color: ${dedfe0}">
                 <th>${title}</th>
             </thead>
